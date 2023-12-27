c6509a654134067ae44245627006ca8eb7910ddf
Ahmad Sghaier <asghaier76@gmail.com>
2023-12-27 13:02:46 -0500

    Adding SBT contract and unit tests
    The contract supports simple SBT that allows minting and owner to burn but no transfer
    Unit test check for basic functions for deploy and getting main contract values and supported interfaces
    then check scenarios of minting, buringing and transfer with certain possible reverts

---
+++ hardhat/contracts/SoulBoundToken.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract SoulBoundToken is ERC721, AccessControl {
+    error NonTransferableToken(uint256 tokenId);
+    error NonTokenOwner(address sender, uint256 tokenId);
+    
+    uint256 private _tokenIdCounter;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(string memory _name, string memory _symbol, address _minter) ERC721(_name, _symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, _minter);
+    }
+
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+        _safeMint(to, tokenId);
+    }
+
+    function burn(uint256 tokenId) external {
+        if(ownerOf(tokenId) != msg.sender) {
+            revert NonTokenOwner(msg.sender, tokenId); 
+        }
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721) returns (address) {
+        address from = _ownerOf(tokenId);
+        if(!(from == address(0) || to == address(0))) {
+            revert NonTransferableToken(tokenId);
+        }
+        return super._update(to, tokenId, auth);
+    }
+}
---
+++ hardhat/test/SoulBoundToken.test.ts
@@ -0,0 +1,118 @@
+const { ethers } = require("hardhat");
+import { expect } from 'chai';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+import { SoulBoundToken } from '../typechain-types';
+
+describe("SoulBoundToken Contract", function () {
+  let soulBoundToken: SoulBoundToken;
+  let contract: any;
+  let owner: SignerWithAddress;
+  let minter: SignerWithAddress;
+  let signers: SignerWithAddress[];
+
+  beforeEach(async function () {
+    soulBoundToken = await ethers.getContractFactory("SoulBoundToken");
+    [owner, minter, ...signers] = await ethers.getSigners();
+    const proxy = await ethers.getContractFactory('SoulBoundToken');
+    contract = (await proxy.deploy(
+        'SoulBoundToken',
+        'SBT',
+        minter
+    )) as unknown as SoulBoundToken;
+    const sbt = contract.deploymentTransaction();
+    await sbt?.wait();
+
+  });
+
+  describe("Deployment", function() {
+    it('Should check contract name and symbol values', async () => {
+        expect(await contract.name()).to.equal('SoulBoundToken');
+        expect(await contract.symbol()).to.equal('SBT');
+    });
+
+    it('Should check contract default admin role assigned to deployer', async () => {
+        const defaultAdminRole = await contract.DEFAULT_ADMIN_ROLE();
+        expect(await contract.hasRole(defaultAdminRole, owner.address)).to.equal(true);    
+    });
+
+    it('Should check contract minter role assigned to minter', async () => {
+        const minterRole = await contract.MINTER_ROLE();
+        expect(await contract.hasRole(minterRole, minter.address)).to.equal(true);    
+    });
+
+  });
+
+  describe('Interfaces', async () => {
+    it('Should check that contracts supports ERC721 interface', async () => {
+      const ERC721Interface = '0x80ac58cd';
+      expect(await contract.supportsInterface(ERC721Interface)).to.equal(true);
+    });
+
+    it('Should check that contracts supports ERC165 interface', async () => {
+      const ERC165Interface = '0x01ffc9a7';
+      expect(await contract.supportsInterface(ERC165Interface)).to.equal(true);
+    });
+
+    it('Should check that contracts supports AccessControl interface', async () => {
+      const AccessControlInterface = '0x7965db0b';
+      expect(await contract.supportsInterface(AccessControlInterface)).to.equal(true);
+    });
+  });
+
+  describe("Minting", function () {
+    it("Should mint a new token to a specified address", async function () {
+      await expect(contract.connect(minter).safeMint(signers[0].address))
+        .to.emit(contract, 'Transfer')
+        .withArgs("0x0000000000000000000000000000000000000000", signers[0].address, 0);
+      expect(await contract.ownerOf(0)).to.equal(signers[0].address);
+    });
+
+    it("Should only allow the owner to mint tokens", async function () {
+      const minterRole = await contract.MINTER_ROLE();
+      await expect(contract.connect(signers[0]).safeMint(signers[0].address))
+        .to.be.revertedWithCustomError(contract, "AccessControlUnauthorizedAccount")
+        .withArgs(signers[0].address, minterRole);
+    });
+
+  });
+
+  describe("Burning", function () {
+    beforeEach(async function () {
+      await contract.connect(minter).safeMint(signers[1].address);
+    });
+
+    it("Should prevent non-owners from burning the token", async function () {
+        await expect(contract.connect(signers[2]).burn(0))
+            .to.be.revertedWithCustomError(contract, "NonTokenOwner")
+            .withArgs(signers[2].address, 0);
+    });
+
+    it("Should allow the token owner to burn the token", async function () {
+      await expect(contract.connect(signers[1]).burn(0))
+        .to.emit(contract, 'Transfer')
+        .withArgs(signers[1].address, "0x0000000000000000000000000000000000000000", 0);
+    });
+
+  });
+
+  describe("Transfering", function () {
+    beforeEach(async function () {
+      await contract.connect(minter).safeMint(signers[2].address);
+    });
+
+    it("Should prevent owners from transfer SBT token, using transferFrom", async function () {
+        await expect(contract.connect(signers[2]).transferFrom(signers[2].address, signers[3].address, 0))
+            .to.be.revertedWithCustomError(contract, "NonTransferableToken")
+            .withArgs(0);
+    });
+    
+    it("Should prevent owners from transfer SBT token, using safeTransferFrom", async function () {
+        await expect(contract.connect(signers[2]).safeTransferFrom(signers[2].address, signers[3].address, 0))
+            .to.be.revertedWithCustomError(contract, "NonTransferableToken")
+            .withArgs(0);
+    });
+
+  });
+
+});
